73cfb8c6ff96214b495138d6a4f836764da1a1bf
Juanjo Tugores <tugorez@gmail.com>
2020-09-04 11:49:29 -0500

    Refactor statement expression

--- parser/statement_expression.go
+++ parser/statement_expression.go
@@ -6,16 +6,14 @@ import (
 )
 
 func (p *Parser) parseStatementExpression() (statement ast.StatementExpression, err error) {
-	if expression, expressionError := p.parseExpression(LOWEST); expressionError == nil {
-		token := p.token
-		if p.checkIsStatementSeparator() {
-			statement = ast.StatementExpression{Expression: expression}
-		} else {
-			err = fmt.Errorf("[StatementExpression] invalid token %q", token.Value)
-		}
-	} else {
-		err = expressionError
+	expression, err := p.parseExpression(LOWEST)
+	if err != nil {
 		p.checkIsStatementSeparator()
+		return
 	}
-	return
+	if token := p.token; !p.checkIsStatementSeparator() {
+		err = fmt.Errorf("[StatementExpression] invalid token %q", token.Value)
+		return
+	}
+	return ast.StatementExpression{Expression: expression}, nil
 }
